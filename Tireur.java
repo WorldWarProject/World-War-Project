8b30a0ac5d5274d4c7c57b5fdb6b6e4a8a2810c3
Hasarin <Shikiryuho.tm@gmail.com>
2015-02-24 20:02:28 +0100

    Create Tireur.java

---
+++ Tireur.java
@@ -0,0 +1,7 @@
+
+public class Tireur extends Robots {
+
+	public Tireur(String affich, int po, int dep, int energie, int degat, int coutAtt, int coutDep) {
+		super(affich, po, dep, energie, degat, coutAtt, coutDep);
+	}
+}
