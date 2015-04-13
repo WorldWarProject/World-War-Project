135b37f3501597d57046753436a2e9d6d1a18c09
Noopyx <quentin.porion@gmail.com>
2015-04-13 18:54:35 +0200

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,29 +1,9 @@
 
-public class Obstacle {
-
-	private int x,y;
-	
+public class Obstacle extends Cellule{
 	public Obstacle(int x, int y) {
-		this.setX(x);
-		this.setY(y);
+		super(x,y,false,0,false,false);
 	}
 
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-	
 	public String toString(){
 		return "O";
 	}
