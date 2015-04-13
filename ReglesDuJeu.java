4697a7064459e192cd7fc752b6c8a85553203886
Hasarin <Shikiryuho.tm@gmail.com>
2015-04-13 22:12:07 +0200

    Update ReglesDuJeu.java

--- ReglesDuJeu.java
+++ ReglesDuJeu.java
@@ -1,17 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 
 public class ReglesDuJeu {
 		private Scanner str;
 	public ReglesDuJeu(){
-		System.out.println("Voici les regles du jeu :\n");
-		System.out.println("- Les pays s'affrontent en faisant combattre des robots sur un plateau.\n");
-		System.out.println("- Le jeu se deroule tour par tour, une equipe est choisie aleatoirement pour commencer la partie.\n");
-		System.out.println("- Au depart les robots se trouvent dans leur base respective.\n");
-		System.out.println("- A chaque tour de jeu, chaque equipe chosit un de ses robots pour realiser une action (deplacement ou attaque).\n");
-		System.out.println("- Une equipe ne peut pas passer son tour, tant qu'un de ses robots possede assez d'energie pour realiser une action elle doit jouer.\n");
-		System.out.println("- Au cours de la partie chaque equipe doit conserver au moins un robot hors de sa base\n");
-		System.out.println("- La partie se termine des qu'une des deux equipes ne possede plus de robot vivant.\n\n");
+		String fichier ="Regles/Regles.txt";
+			
+		try{
+			InputStream fichips=new FileInputStream(fichier); 
+			InputStreamReader fichipsr=new InputStreamReader(fichips);
+			BufferedReader fichbr=new BufferedReader(fichipsr);
+			String ligne;
+			while ((ligne=fichbr.readLine())!=null){
+				System.out.println(ligne+"\n");
+				}
+			fichbr.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+		    
+		   
+		  
+
 		System.out.println("Pour retourner au menu tapez 1");
 		str = new Scanner(System.in);
 		 /**
