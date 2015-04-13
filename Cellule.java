aed9f63760fc34a120e7c9f4afdf9777bec3364d
Noopyx <quentin.porion@gmail.com>
2015-04-13 18:53:55 +0200

    Create Cellule.java

---
+++ Cellule.java
@@ -0,0 +1,51 @@
+
+public abstract class Cellule extends Coordonne{
+	protected boolean mine = false, base =false, robot =false;
+	protected int equipe = 0;
+	
+	public Cellule(int x, int y) {
+		super(x, y);
+	}
+	
+	public Cellule(int x, int y, boolean mine, int equipe,  boolean base, boolean robot) {
+		super(x, y);
+		this.equipe = equipe;
+		this.mine = mine;
+		this.base = base;
+		this.robot = robot;
+	}
+	
+	public boolean isRobot() {
+		return robot;
+	}
+
+	public void setRobot(boolean robot) {
+		this.robot = robot;
+	}
+
+	public boolean isMine() {
+		return mine;
+	}
+
+	public void setMine(boolean mine) {
+		this.mine = mine;
+	}
+
+	public int getEquipe() {
+		return equipe;
+	}
+
+	public void setEquipe(int equipe) {
+		this.equipe = equipe;
+	}
+
+	public boolean isBase() {
+		return base;
+	}
+
+	public void setBase(boolean base) {
+		this.base = base;
+	}
+
+	
+}
