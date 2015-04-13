6c16730f422fcde57653299a893856f27ded9cdd
Noopyx <quentin.porion@gmail.com>
2015-04-13 18:56:25 +0200

    Update PathFinding.java

--- PathFinding.java
+++ PathFinding.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 public class PathFinding {
 
-	private Coordonne[][] tab;
-	private Coordonne start, goal;
-	private List<Coordonne> path = new ArrayList<Coordonne>();
+	private CoordonnePF[][] tab;
+	private CoordonnePF start, goal;
+	private List<CoordonnePF> path = new ArrayList<CoordonnePF>();
 	
-	public PathFinding (Coordonne start, Coordonne goal) {
+	public PathFinding (CoordonnePF start, CoordonnePF goal) {
 		this.start = start;
 		this.goal = goal;
 	}
 
-	public Coordonne[][] getTab() {
+	public CoordonnePF[][] getTab() {
 		return this.tab;
 	}
 
 	public void setTab(Object[][] tab) {
-		this.tab= new Coordonne[tab.length][tab[0].length];
+		this.tab= new CoordonnePF[tab.length][tab[0].length];
 		for (int i=0; i<tab.length; i++) {
 			for(int j=0; j<tab[0].length; j++) {
 				if(!(tab[i][j] instanceof Obstacle)) {
-					this.tab[i][j] = new Coordonne(j,i);
+					this.tab[i][j] = new CoordonnePF(j,i);
 					this.tab[i][j].setH(Math.abs((j-this.goal.getX())+(i-this.goal.getY())));
 				}
 				else {
-					this.tab[i][j] = new Coordonne(j,i);
+					this.tab[i][j] = new CoordonnePF(j,i);
 					this.tab[i][j].setH(-1);
 				}
 			}
@@ -33,28 +33,28 @@ public class PathFinding {
 	
 	public boolean findPath() {
 		if(this.tab != null) {// mettre un try and catch avec start et goal
-			boolean wrongPath = false;
-			Coordonne tmp = new Coordonne(0,0);
-			Coordonne c = new Coordonne(0,0);
-			List<Coordonne> StartPoint = new ArrayList<Coordonne>();
+			boolean wrongPath = false , wrongStart = false;
+			CoordonnePF tmp = new CoordonnePF(0,0);
+			CoordonnePF c = new CoordonnePF(0,0);
+			List<CoordonnePF> StartPoint = new ArrayList<CoordonnePF>();
 			
 			if (this.start.getX()-1>0){
 				if ( this.start.getX()+1<this.tab[0].length-1) {
 					if (this.tab[this.start.getY()][this.start.getX()-1].getH() > this.tab[this.start.getY()][this.start.getX()+1].getH())
 						for (int i=this.start.getX(); i>0; i--)
-							StartPoint.add(new Coordonne(i,this.start.getY()));
+							StartPoint.add(new CoordonnePF(i,this.start.getY()));
 					else 
 						for (int i=this.start.getX(); i<this.tab[0].length; i++)
-							StartPoint.add(new Coordonne(i,this.start.getY()));
+							StartPoint.add(new CoordonnePF(i,this.start.getY()));
 				}
 				else {
 					for (int i=this.start.getX(); i>0; i--)
-						StartPoint.add(new Coordonne(i,this.start.getY()));
+						StartPoint.add(new CoordonnePF(i,this.start.getY()));
 				}	
 			}
 			else {
 				for (int i=this.start.getX(); i<this.tab[0].length; i++)
-					StartPoint.add(new Coordonne(i,this.start.getY()));
+					StartPoint.add(new CoordonnePF(i,this.start.getY()));
 			}
 			
 			
@@ -62,49 +62,62 @@ public class PathFinding {
 				if ( this.start.getY()+1<this.tab.length-1) {
 					if (this.tab[this.start.getY()][this.start.getX()-1].getH() > this.tab[this.start.getY()][this.start.getX()+1].getH())
 						for (int i=this.start.getY(); i>0; i--)
-							StartPoint.add(new Coordonne(this.start.getX(),i));
+							StartPoint.add(new CoordonnePF(this.start.getX(),i));
 					else 
 						for (int i=this.start.getY(); i<this.tab.length; i++)
-							StartPoint.add(new Coordonne(this.start.getX(),i));
+							StartPoint.add(new CoordonnePF(this.start.getX(),i));
 				}
 				else {
 					for (int i=this.start.getY(); i>0; i--)
-						StartPoint.add(new Coordonne(this.start.getX(),i));
+						StartPoint.add(new CoordonnePF(this.start.getX(),i));
 				}	
 			}
 			else {
 				for (int i=this.start.getY(); i<this.tab.length; i++)
-					StartPoint.add(new Coordonne(this.start.getX(),i));
+					StartPoint.add(new CoordonnePF(this.start.getX(),i));
 			}
 			
 			
-			for (Coordonne co : StartPoint){
+			for (CoordonnePF co : StartPoint){
 				c = co;
 				int h=tab[c.getY()][c.getX()].getH();
 				wrongPath = false;
+				wrongStart =false;
+				if(co.getX()==0) {
+					for(int i=co.getY(); i>0 ; i--)
+						if (this.tab[i][co.getX()].getH() == -1)
+							wrongStart = true;
+				}
+				else {
+					for(int i=co.getX();i>0; i--)
+						if (this.tab[co.getY()][i].getH() == -1)
+							wrongStart = true;
+				}
+						
+						
+						
 				if (co.getX() > this.start.getX()) 
 					for (int i=this.start.getX(); i<co.getX() ;i++) 
-						this.path.add(new Coordonne(i,co.getY()));
+						this.path.add(new CoordonnePF(i,co.getY()));
 				else if (co.getX()<this.start.getX())
 					for (int i=this.start.getX(); i>co.getX() ;i--)
-						this.path.add(new Coordonne(i,co.getY()));
+						this.path.add(new CoordonnePF(i,co.getY()));
 				
 				if (co.getY() > this.start.getY()) 
 					for (int i=this.start.getY(); i<co.getY() ;i++) 
-						this.path.add(new Coordonne(co.getX(),i));
+						this.path.add(new CoordonnePF(co.getX(),i));
 				else if (co.getY()<this.start.getY())
 					for (int i=this.start.getY(); i>co.getY() ;i--)
-						this.path.add(new Coordonne(co.getX(),i));
+						this.path.add(new CoordonnePF(co.getX(),i));
 				while((c.getX() != this.goal.getX() || c.getY() != this.goal.getY()) && !wrongPath ) {
 					wrongPath = true;
 					this.path.add(c);			
-					System.out.println(c);
 					for (int i=-1; i<=1 ;i++) {
 						for (int j=-1; j<=1; j++) {
 							if(c.getX()+i >= 0 && c.getX()+i< this.tab[0].length && c.getY()+j>=0 && c.getY()+j<this.tab.length) 
 								if(this.tab[c.getY()+j][c.getX()+i].getH()< h && this.tab[c.getY()+j][c.getX()+i].getH() != -1) {
 								h = this.tab[c.getY()+j][c.getX()+i].getH();
-									tmp = new Coordonne(c.getX()+i,c.getY()+j);
+									tmp = new CoordonnePF(c.getX()+i,c.getY()+j);
 									wrongPath = false;
 								}
 						
@@ -115,7 +128,10 @@ public class PathFinding {
 					if(wrongPath) 
 						this.path.clear();
 				}
-				if(!wrongPath) {
+				if(wrongStart){
+					this.path.clear();
+				}
+				if(!wrongPath && !wrongStart) {
 					this.path.add(c);
 					return true;
 				}
@@ -144,7 +160,7 @@ public class PathFinding {
 		return msg;
 	}
 
-	public List<Coordonne> getPath() {
+	public List<CoordonnePF> getPath() {
 		return path;
 	}
 	
