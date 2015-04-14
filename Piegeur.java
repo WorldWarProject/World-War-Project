1974fad0084f5aaf455df4baaa811ee95e5e27f7
Noopyx <quentin.porion@gmail.com>
2015-04-14 18:22:07 +0200

    Create Piegeur.java

---
+++ Piegeur.java
@@ -0,0 +1,75 @@
+public class Piegeur extends Robot {
+
+	public Piegeur(int equipe) {
+		super(equipe, Constante.ENERGIE_PIEGEUR);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean estSurBase() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getEquipe() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRegenEnergie() {
+		// TODO Auto-generated method stub
+		return Constante.REGEN_ENERGIE_PIEGEUR;
+	}
+
+	@Override
+	public int getCoutDeplacement() {
+		// TODO Auto-generated method stub
+		return Constante.COUT_AVANCER_PIEGEUR;
+	}
+
+	@Override
+	public int getCoutTir() {
+		// TODO Auto-generated method stub
+		return Constante.COUT_TIRER_PIEGEUR;
+	}
+
+	@Override
+	public int getDegatTir() {
+		// TODO Auto-generated method stub
+		return Constante.DEGAT_PIEGEUR;
+	}
+
+	@Override
+	public int getDegatMine() {
+		// TODO Auto-generated method stub
+		return Constante.DEGAT_PIEGEUR;
+	}
+
+	@Override
+	public int getPortee() {
+		// TODO Auto-generated method stub
+		return Constante.PORTEE_PIEGEUR;
+	}
+
+	public int getCoutMine() {
+		// TODO Auto-generated method stub
+		return Constante.COUT_MINER;
+	}
+
+	@Override
+	public String getType() {
+		if (super.getEquipe()==1)
+			return Constante.NOM_PIEGEUR;
+		else
+			return Constante.NOM_PIEGEUR_EQUIPE2;
+	}
+
+	@Override
+	public int getPorteeDeplacement() {
+		// TODO Auto-generated method stub
+		return Constante.DEPLACEMENT_PIEGEUR;
+	}
+
+}
