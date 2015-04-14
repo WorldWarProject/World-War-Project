e56534903fb2cb4da0e887717176386d14355e51
Noopyx <quentin.porion@gmail.com>
2015-04-14 18:17:56 +0200

    Update Base.java

--- Base.java
+++ Base.java
@@ -1,31 +1,53 @@
 
+
 public class Base extends Cellule{
-	private Robots[] B = new Robots[5];
+	private Robot[] B = new Robot[5];
 	private int idx = 0;
 
 	public Base(int x, int y, int equipe) {
 		super(x, y, false, equipe, true, false);
 	}
 
-	public String getB() {
-		String msg ="";
-		int cpt =0;
-		while(this.B[cpt] != null)
-			msg+=this.B[cpt].toString()+"\n";
-		return msg;
+	public Robot[] getB() {
+		return this.B;
 	}
 	
-	public void add(Robots o) {
-		this.B[this.idx]=o;
-		this.idx++;
+	public void add(Robot r) {
+		if(idx <5) {
+			this.B[this.idx]=r;
+			this.idx++;
+		}
 	}
 	
 	public void remove(int idx) {
-		this.B[idx]=null;
+		boolean decal = false;
+		for (int i =0; i<this.idx ; i++) {
+			if (decal)
+				this.B[i-1]=this.B[i];
+			if (i == idx)
+				decal = true;	
+		}
+		this.B[this.idx]=null;
+		this.idx--;
+	}
+	
+	public int getIdx() {
+		return this.idx;
 	}
 	
 	public String toString () {
 		if (this.equipe == 1) return "B";
 		else return "b";
+	}
+
+	public void setR(Robot r) {		
+	}
+
+	public void remove() {
+		this.B = new Robot[5];
+	}
+
+	public Robot getR() {
+		return null;
 	}
 }
