b4a2c1017c9db8b8205a1822b6ba4eb5211d9fcf
Hasarin <Shikiryuho.tm@gmail.com>
2015-04-14 22:23:59 +0200

    Update Jouer.java

--- Jouer.java
+++ Jouer.java
@@ -3,23 +3,67 @@ import java.util.Scanner;
 
 public class Jouer {
 	private Scanner str;
-	private int x;
-	private int y;
-	private int z;
+	private String x;
+	private String y;
+	private String o;
+	
+	
+	
 	
 	public Jouer(){
 		str = new Scanner(System.in);
 		System.out.println("Création du plateau :");
-		System.out.println("Le nombre de Colonne et de ligne doit être compris entre 2 et 20");
-		System.out.println("Nombre de Colonne:");
-		x = str.nextInt();
-		System.out.println("Nombre de ligne:");
-		y = str.nextInt();
-		z = 1;
-		System.out.println("Tour de jeu numéro "+z);
-		Plateau jeu = new Plateau(x,y);
+		creationColonne();
+		creationLigne();
+		creationObstacle();
+		System.out.println("Tour de jeu numéro ");
+		Plateau jeu = new Plateau(Integer.parseInt(x),Integer.parseInt(y),Integer.parseInt(o));
 		System.out.println(jeu);
 		
-	
+		
 	}
-}
+	
+	public void creationColonne(){
+		try{
+		do{
+		System.out.println("Le nombre de Colonne doit être compris entre 10 et 20");
+		System.out.println("Nombre de Colonne:");
+		x = str.nextLine();}
+		while(Integer.parseInt(x)<10 || Integer.parseInt(x)>20);
+		}catch(Exception e){
+			creationColonne();
+		}
+	}
+	
+	public void creationLigne(){
+		try{
+		do{
+			System.out.println("Le nombre de Ligne doit être compris entre 10 et 20");
+			System.out.println("Nombre de ligne:");
+			y = str.nextLine();
+			}
+			while(Integer.parseInt(y)<10 || Integer.parseInt(y)>20);
+		}catch(Exception e){
+			creationLigne();
+		}
+	}
+		
+		public void creationObstacle(){
+			try{
+			do{
+				System.out.println("Veuillez saisir le pourcentage d'obstacle que vous souhaitez");
+				System.out.println("Le pourcentage d'obstacle ne peut être supérieur à 25%");
+				System.out.println("Pourcentage d'obstacle:");
+				o = str.nextLine();}
+			while(Integer.parseInt(o)<0||Integer.parseInt(o)>25);
+			}catch(Exception e){
+				creationObstacle();
+			}
+		}  
+		
+		
+		public boolean findepartie(){
+			return false;
+		}
+	}
+
