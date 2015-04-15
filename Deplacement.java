93b7fd9900b2a31f29367eb731c2e4815383955c
Noopyx <quentin.porion@gmail.com>
2015-04-15 13:35:20 +0200

    Update Deplacement.java

--- Deplacement.java
+++ Deplacement.java
@@ -1,37 +1,133 @@
 
 public class Deplacement {
 
-	public static boolean isPossible(Cellule start, Cellule goal) {
-		if(start.isRobot() && !goal.isRobot() && !(goal instanceof Obstacle))
-			if((Math.abs(start.getX()-goal.getX()) + Math.abs(start.getY()-goal.getY())) <= start.getR().getPorteeDeplacement()){
-				start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
-				return true;
-			}
+	public static boolean isPossible(Cellule start, Cellule goal,Cellule[][] tab) {
+		if(!(start.getR() instanceof Char)) {
+			if(start.isRobot() && !goal.isRobot() && !(goal instanceof Obstacle))
+				if((Math.abs(start.getX()-goal.getX())<= start.getR().getPorteeDeplacement() && Math.abs(start.getY()-goal.getY())<= start.getR().getPorteeDeplacement())){
+					start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+					return true;
+				}
+				else return false;
 			else return false;
-		else return false;
-		//portee DEplacment / perte énergie
+		}
+		else {
+			if(start.isRobot() && !goal.isRobot() && !(goal instanceof Obstacle)){
+				if ((start.getX() == goal.getX()||(start.getY() == goal.getY()) && (Math.abs(start.getX()-goal.getX())<= start.getR().getPorteeDeplacement() && Math.abs(start.getY()-goal.getY()) <= start.getR().getPorteeDeplacement()))){
+						if(Math.abs(start.getX()-goal.getX()) == 2 || Math.abs(start.getY()-goal.getY()) == 2){
+							if(start.getX()-goal.getX() < 0) {
+								if (tab[start.getX()+1][start.getY()] instanceof Obstacle)
+									return false;
+								else {
+									start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+									return true;
+								}
+									
+							}
+							else if (start.getX()-goal.getX() > 0){
+								if (tab[start.getX()-1][start.getY()] instanceof Obstacle)
+									return false;
+								else {
+									start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+									return true;
+								}
+							}
+							else if (start.getY()-goal.getY() < 0){
+								if (tab[start.getX()][start.getY()+1] instanceof Obstacle)
+									return false;
+								else {
+									start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+									return true;
+								}
+							}
+							else if (start.getX()-goal.getX() > 0){
+								if (tab[start.getX()][start.getY()-1] instanceof Obstacle)
+									return false;
+								else {
+									start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+									return true;
+								}
+							}
+							else return false;							
+						}
+						else {
+							start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+							return true;
+						}
+				}
+			}
+		}
+		return false;
+		
 	}
 	
-	public static boolean isPossible(Cellule start, Cellule goal,int idx) {
+	public static boolean isPossible(Cellule start, Cellule goal,int idx , Cellule[][] tab) {
 		Robot[] r = start.getB();
-		if(!goal.isRobot() && !(goal instanceof Obstacle))
-			if((Math.abs(start.getX()-goal.getX()) + Math.abs(start.getY()-goal.getY())) <= r[idx].getPorteeDeplacement()){
-				r[idx].setEnergie(r[idx].getEnergie()-r[idx].getCoutDeplacement());
-				return true;
-			}
+		if(!(r[idx] instanceof Char)) {
+			if(!goal.isRobot() && !(goal instanceof Obstacle))
+				if((Math.abs(start.getX()-goal.getX())== r[idx].getPorteeDeplacement() || Math.abs(start.getY()-goal.getY())== r[idx].getPorteeDeplacement())){
+					r[idx].setEnergie(r[idx].getEnergie()-r[idx].getCoutDeplacement());
+					return true;
+				}
+				else return false;
 			else return false;
-		else return false;
+		}
+		else {
+			if(!goal.isRobot() && !(goal instanceof Obstacle))
+				if ((start.getX() == goal.getX()||(start.getY() == goal.getY()) && (Math.abs(start.getX()-goal.getX())<= r[idx].getPorteeDeplacement() || Math.abs(start.getY()-goal.getY()) <= r[idx].getPorteeDeplacement())))
+						if(Math.abs(start.getX()-goal.getX()) == 2 || Math.abs(start.getY()-goal.getY()) == 2){
+							if(start.getX()-goal.getX() < 0) {
+								if (tab[start.getX()+1][start.getY()] instanceof Obstacle)
+									return false;
+								else {
+									start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+									return true;
+								}
+							}
+							else if (start.getX()-goal.getX() > 0){
+								if (tab[start.getX()-1][start.getY()] instanceof Obstacle)
+									return false;
+								else {
+									start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+									return true;
+								}
+							}
+							else if (start.getY()-goal.getY() < 0){
+								if (tab[start.getX()][start.getY()+1] instanceof Obstacle)
+									return false;
+								else {
+									start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+									return true;
+								}
+							}
+							else if (start.getX()-goal.getX() > 0){
+								if (tab[start.getX()][start.getY()-1] instanceof Obstacle)
+									return false;
+								else {
+									start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+									return true;
+								}
+							}
+							else return false;							
+						}
+						else {
+							start.getR().setEnergie(start.getR().getEnergie()-start.getR().getCoutDeplacement());
+							return true;
+						}
+		}
+		return false;
+		
 	}
 	
-	public static Coordonne deplacement(Cellule start, Cellule goal) {
-		if (isPossible(start,goal))
+	public static Coordonne deplacement(Cellule start, Cellule goal , Cellule[][] tab) {
+		if (isPossible(start,goal,tab))
 				return new Coordonne(goal.getX(),goal.getY());
 		else
 			return null;
 	}
 	
-	public static Coordonne deplacement(Cellule start, Cellule goal,int idx) {
-		if (isPossible(start,goal,idx))
+	public static Coordonne deplacement(Cellule start, Cellule goal,int idx , Cellule[][] tab) {
+		if (isPossible(start,goal,idx,tab))
 				return new Coordonne(goal.getX(),goal.getY());
 		else
 			return null;
