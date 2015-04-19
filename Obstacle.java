25e122dcccd5ff30e4e5fce957aea27ce9022063
guervild <dylan.guerville@hotmail.fr>
2015-04-19 15:03:55 +0200

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,13 +1,22 @@
-
+/**
+ * La classe obstacle represente un obstacle heritant de cellule et de ses parametres
+ * @author guervild
+ *
+ */
 public class Obstacle extends Cellule{
+	/**
+	 * Construit un objet obstacle avec ses coordonnée en x et en y
+	 * @param x - coordonnée en x de l'obstacle
+	 * @param y - coordonnée en y de l'obstacle
+	 */
 	public Obstacle(int x, int y) {
 		super(x,y,false,0,false,false);
 	}
-
+	@Override
 	public String toString(){
 		return "O";
 	}
-
+	
 	public Robot[] getB() {
 		return null;
 	}
