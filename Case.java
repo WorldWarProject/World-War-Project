f3ce1b7045e5be2e63d4c56269940a19648ebc12
guervild <dylan.guerville@hotmail.fr>
2015-04-19 15:00:45 +0200

    Update Case.java

--- Case.java
+++ Case.java
@@ -1,15 +1,38 @@
+/**
+ * La classe case represente une case du tableau, herite de cellule et de ses parametres.
+ * @author guervild
+ */
 
 public class Case extends Cellule {
+	/**
+	 * Represente un robot
+	 */
 	protected Robot r;
 	
+	/**
+	 * Construit une case avec ses coordonnée en x,y
+	 * @param x - coordonnée en x
+	 * @param y - coordonnée en y
+	 */
 	public Case(int x, int y) {
 		super(x, y);
 	}
 
+	/**
+	 *  Construit une case avec ses coordonnée en x,y, la presence d'une mine ou non,
+	 *   et son equipe.
+	 * @param x -coordonnée en x
+	 * @param y - coordonnée en y
+	 * @param mine - la presence d'une mine
+	 * @param equipe - l'equipe du robot present sur la case
+	 */
 	public Case(int x, int y, boolean mine , int equipe) {
 		super(x, y,mine , equipe, false, false);
 	}
 	
+	/**
+	 * Ajoute un robot dans la case
+	 */
 	public void add(Robot r) {
 		this.r = r;
 		if (r != null)
@@ -18,10 +41,16 @@ public class Case extends Cellule {
 			super.setRobot(false);
 	}
 	
+	
+	/**
+	 * Retourne le robot present dans la case
+	 * return - le robot present dans la case
+	 */
 	public Robot getR() {
 		return this.r;
 	}
 	
+	@Override
 	public String toString () {
 		if(this.r == null)
 			return " ";
@@ -36,7 +65,9 @@ public class Case extends Cellule {
 		return 0;
 	}
 
-
+	/**
+	 * Suprime le robot de la case
+	 */
 	public void remove() {
 		super.setRobot(false);
 		this.r = null;
