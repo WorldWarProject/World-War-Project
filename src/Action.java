7a3999fdf3a51a9befbabde9d79597162af6f10e
murschet <murschet@localhost.localdomain>
2015-04-27 22:50:23 +0200

    action

--- src/Action.java
+++ src/Action.java
@@ -25,29 +25,49 @@ public class Action {
 		System.out.println("Choisissez un des robots suivants:");
 		for (int i=0;i<E1.E.size();i++){
 		System.out.println(i+". "+ E1.getE().get(i)+" "+E1.getE().get(i).getCoordonne());}
+		System.out.println(E1.getE().size()+". Retour au menu action");
 		cr=sc.nextLine();
+		if (Integer.parseInt(cr)==E1.getE().size())
+			new Action(jeu, E1, tab);
+		else
 		new Deplacement(jeu,E1,Integer.parseInt(cr),tab);
 		
 		}else if (ch.equals("2")){
+			System.out.println("Equipe "+E1.getNom());
+			System.out.println("Choisissez un des robots suivants:");
+			for (int i=0;i<E1.E.size();i++){
+			System.out.println(i+". "+ E1.getE().get(i)+" "+E1.getE().get(i).getCoordonne());}
+			System.out.println(E1.getE().size()+". Retour au menu action");
+			cr=sc.nextLine();
+			if(E1.getE().get(Integer.parseInt(cr)).getType()=="C");
+			new Tirer();
+			
 		
 			}
 		else if(ch.equals("3")){
 			
 			 if(E1.getE().size()==1){
-					if(E1.getE().get(0).estSurBase(E1, 0, tab)==true && E1.getE().get(0).getEnergie()!=0){
+					if(E1.getE().get(0).estSurBase(E1, 0, tab)==true && E1.getE().get(0).getEnergie()==0){
+						
+					}else{	
 						System.out.println("Vous ne pouvez pas passer votre tour");
 						System.out.println(jeu);
-						new Action (jeu,E1,tab);
-					}else if(E1.getE().get(0).estSurBase(E1, 0, tab)==false){
-						System.out.println("Vous ne pouvez pas passer votre tour");
-						System.out.println(jeu);
-						new Action (jeu,E1,tab);
-					}
-			 }
-		}else{
+						new Action (jeu,E1,tab);}
+			 
+			 }else{
+				System.out.println("Vous ne pouvez pas passer votre tour");
+				System.out.println(jeu);
+				new Action (jeu,E1,tab);}
+		}else if (ch.equals("4")){
+			for(int i = 0;i<E1.getE().size();i++){
+				E1.getE().clear();
+			}
 			
-			new Action(jeu, E1,tab);
-		}
+		}else{
+			System.out.println("choix invalide");
+			new Action (jeu,E1,tab);
+			}
+				
 		}catch(Exception e){
 			System.out.println(e);
 			new Action (jeu,E1,tab);
